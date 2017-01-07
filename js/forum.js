bab5ef1d77389454b661b3de734b238d25256334
Serghei Iakovlev <serghei@phalconphp.com>
2017-01-07 12:48:24 +0200

    Added JS for #header-toolbar

---
+++ js/forum.js
@@ -0,0 +1,43 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        //noinspection JSValidateTypes
+        var $wn               = $(window),
+            $headerEl         = $('#header'),
+            $headerToolbar    = $('#header-toolbar'),
+            $headerNavEl      = $('.header-toolbar-nav'),
+            headerH           = $headerEl.outerHeight(),
+            $headerNavLinks   = $('.header-toolbar-nav [data-toggle="tab"]'),
+            $headerNavTab     = $('.header-toolbar-nav-tab'),
+            $headerNavTabPane = $('.header-toolbar-nav-tab-pane'),
+            headerNavTabPaneH = $headerNavTabPane.outerHeight();
+
+        $headerNavLinks.on('click', function () {
+            var $t          = $(this),
+                $tp         = $(this).parent('li'),
+                toggleClass = 'active',
+                curHref     = $t.attr('href'),
+                $targetEl   = $(curHref);
+
+            if ($tp.hasClass(toggleClass)) {
+                $tp.removeClass(toggleClass);
+                $targetEl.slideUp();
+
+                //noinspection JSValidateTypes
+                $headerToolbar.css('height', ($headerNavEl.outerHeight() - $headerNavTab.outerHeight()));
+            } else {
+                $tp.siblings().removeClass(toggleClass);
+                $tp.addClass(toggleClass);
+
+                $targetEl.siblings().slideUp();
+                $targetEl.slideDown();
+
+                if (!$headerToolbar.hasClass('is-sticky')) {
+                    //noinspection JSValidateTypes
+                    $headerToolbar.css('height', ($headerNavEl.outerHeight() + headerNavTabPaneH));
+                }
+            }
+        });
+    });
+})(jQuery);
